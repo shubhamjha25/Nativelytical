55dc50ec0bd5c21f48a520e2d13e0fb214ac91b5
Shubham Jha <shubham.jha2001m@gmail.com>
2021-05-09 23:21:47 +0530

    Using State

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+    const [name, setName] = useState('Shubham');
+
+    const clickHandler = () => {
+        setName('Elon Musk')
+    }
+
     return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.boldText}>Hello, World!</Text>
+            <Text>My name is {name}</Text>
+            <Text></Text>
+
+            <View style={styles.buttonContainer}>
+                <Button title="Update State" onPress={clickHandler} />
             </View>
-            <View style={styles.body}>
-                <Text>Lorem ipsum dolor sit amet.</Text>
-                <Text>Lorem ipsum dolor sit amet.</Text>
-                <Text>Lorem ipsum dolor sit amet.</Text>
-            </View>
+
             <StatusBar style="auto" />
         </View>
     );
@@ -25,15 +31,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    header: {
-        backgroundColor: 'pink',
-        padding: 20,
-    },
-    boldText: {
-        fontWeight: 'bold'
-    },
-    body: {
-        backgroundColor: 'yellow',
-        padding: 20
-    } 
+    buttonContainer: {
+        marginTop: 20
+    }
 });
