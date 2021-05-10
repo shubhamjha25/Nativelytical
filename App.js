21a7635091cdc4a862ebcd3880d2b754de217317
Shubham Jha <shubham.jha2001m@gmail.com>
2021-05-10 20:34:16 +0530

    Lists & ScrollView

--- App.js
+++ App.js
@@ -1,34 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
 
-    const [name, setName] = useState('Shubham');
-    const [age, setAge] = useState(20);
-
-    const clickHandler = () => {
-        setName('Elon Musk')
-    }
+    const [people, setPeople] = useState([
+        { name: 'Shubham', key: '1' },
+        { name: 'Elon', key: '2' },
+        { name: 'Alastair', key: '3' },
+        { name: 'Doge', key: '4' },
+        { name: 'Malan', key: '5' },
+        { name: 'Nortje', key: '6' },
+        { name: 'Smith', key: '7' },
+    ]);
 
     return (
         <View style={styles.container}>
-            
-            <Text>Enter Name : </Text>
-            <TextInput 
-                style={styles.input} 
-                placeholder="e.g. John Doe" 
-                onChangeText={(val) => setName(val)} />
-
-            <Text>Enter Age : </Text>
-            <TextInput 
-                style={styles.input} 
-                placeholder="e.g. 30"
-                keyboardType='numeric' 
-                onChangeText={(val) => setAge(val)} /> 
-
-            <Text>Name: {name}, Age: {age}</Text>
-
+            <ScrollView>
+                {
+                    people.map((item) => {
+                        return (
+                            <View key={item.key}>
+                                <Text style={styles.item}>{item.name}</Text>
+                            </View>
+                        )
+                    })
+                }
+            </ScrollView>
             <StatusBar style="auto" />
         </View>
     );
@@ -38,14 +36,15 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        paddingTop: 40,
+        paddingHorizontal: 20
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
-    input: {
-        borderWidth: 1,
-        borderColor: '#777',
-        padding: 8,
-        margin: 10,
-        width: 200
+    item: {
+        marginTop: 24,
+        padding: 30,
+        fontSize: 24,
+        backgroundColor: 'pink'
     }
 });
