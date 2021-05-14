dc927ba67a623145172f0d9a77a4e8e0b51391c9
Shubham Jha <shubham.jha2001m@gmail.com>
2021-05-14 19:45:38 +0530

    Icons & More Flexbox

--- components/ToDoItem.js
+++ components/ToDoItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const ToDoItem = (props) => {
     return (
         <TouchableOpacity onPress={() => props.pressHandler(props.item.key)} >
-            <Text style={styles.item}>{props.item.text}</Text>
+            <View style={styles.item}>
+                <MaterialIcons name='delete' size={22} color={'#333'} />
+                <Text style={styles.itemText}>{props.item.text}</Text>
+            </View>            
         </TouchableOpacity>
     );
 }
@@ -16,8 +20,13 @@ const styles = StyleSheet.create({
         borderColor: '#bbb',
         borderWidth: 1,
         borderStyle: 'dashed',
-        borderRadius: 10
-    }
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    itemText: {
+        marginLeft: 10
+    },
 });
 
 export default ToDoItem;
